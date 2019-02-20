f3c5bf76344ca6cbe3ccbcec5e6fc8a72632aa9c
Aleks Janowska <aej45@cam.ac.uk>
2019-02-20 18:09:02 +0000

    Create motor_functions.ino

---
+++ motor_functions/motor_functions.ino
@@ -0,0 +1,33 @@
+/* 
+ * This file is the motor file.
+ * Keep all functions used to control the motors and servos.
+ */
+
+// Main two motor controls
+void move_forward(){
+}
+
+void move_backward(){
+}
+
+void turn_right(){
+}
+
+void turn_left(){
+}
+
+// Lift motor
+void lift_up(is_max_up){
+  while(is_max_up == False){
+    // lift goes up until hits top of the shaft
+    pass
+  }
+}
+void lift_down(is_max_down){
+  while(is_max_down == False){
+    // lift goes down until hits bottom of the shaft
+    pass
+  }
+}
+
+// Sweep servo
