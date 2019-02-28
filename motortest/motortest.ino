cd2dc6ceddc61baddabc99978984ac877492bf8c
Aleks Janowska <aej45@cam.ac.uk>
2019-02-28 11:10:27 +0000

    setup motor/ultrasound code
    still not working but i gotta go

--- motortest/motortest.ino
+++ motortest/motortest.ino
@@ -1,35 +1,85 @@
 #include <Wire.h>
 #include <Adafruit_MotorShield.h>
-Adafruit_DCMotor *left_motor;
-Adafruit_DCMotor *right_motor;
+Adafruit_MotorShield AFMS = Adafruit_MotorShield();
+Adafruit_DCMotor *leftmotor = AFMS.getMotor(1); // left motor reverse commands
+Adafruit_DCMotor *rightmotor = AFMS.getMotor(2); // right motor normal commands
+const int trigPin = 9;
+const int echoPin = 10;
+long duration;
+float distance;
+bool keepgoing;
+uint8_t i;
+
 
 void setup() {
-  //Serial.begin(9600);           // set up Serial library at 9600 bps
+  pinMode(trigPin, OUTPUT); // Sets the trigPin as an Output
+  pinMode(echoPin, INPUT); // Sets the echoPin as an Input
+  Serial.begin(9600);           // set up Serial library at 9600 bps
 
-  Adafruit_MotorShield AFMS = Adafruit_MotorShield(); // create default I2C address motor shield object
+   // create default I2C address motor shield object
   // custom address in brackets, eg Adafruit_MotorShield(0x61)
-
-  // set motor ports
-  Adafruit_DCMotor *left_motor = AFMS.getMotor(1); // left motor reverse commands
-  Adafruit_DCMotor *right_motor = AFMS.getMotor(2); // right motor normal commands
 
   //AFMS.begin();  // create with the default frequency 1.6KHz
   //AFMS.begin(1000);  // OR with a different frequency, say 1KHz
   
   // Set the speed to start, from 0 (off) to 255 (max speed)
-  right_motor->setSpeed(150);
-  right_motor->run(FORWARD);
+  //right_motor->setSpeed(150);
+  //right_motor->run(FORWARD);
   // turn on motor
-  right_motor->run(RELEASE);
-  left_motor->setSpeed(150);
-  left_motor->run(FORWARD);
+  rightmotor->run(RELEASE);
+  //left_motor->setSpeed(150);
+  //left_motor->run(FORWARD);
   // turn on motor
-  left_motor->run(RELEASE);
-
-  
+  leftmotor->run(RELEASE);
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+  // if right distance to wall is decreasing, speed up right motor
+  // if -----------------------is increasing, speed up left motor
+  // if front distance to wall is x, stop
+  // Clears the trigPin
+  digitalWrite(trigPin, LOW);
+  delayMicroseconds(2);
+  
+  // Sets the trigPin on HIGH state for 10 micro seconds
+  digitalWrite(trigPin, HIGH);
+  delayMicroseconds(10);
+  digitalWrite(trigPin, LOW);
+  
+  // Reads the echoPin, returns the sound wave travel time in microseconds
+  duration = pulseIn(echoPin, HIGH);
+  
+  // Calculating the distance
+  distance= duration*0.034/2;
+
+  
+
+  if(distance>10){
+    Serial.println("INSIDE THE IF!!");
+    rightmotor->setSpeed(0);
+    leftmotor->setSpeed(0);
+    rightmotor->run(FORWARD);
+    leftmotor->run(BACKWARD);
+    delay(1000);
+    
+  }
+  else{
+    Serial.println("INSIDE THE ELSE");
+    rightmotor->setSpeed(100);
+    leftmotor->setSpeed(100);
+    rightmotor->run(BACKWARD);
+    leftmotor->run(FORWARD);
+    
+    delay(1000);
+  }
+
+  //right_motor->run(RELEASE);
+  //left_motor->run(RELEASE);
+
+  
+  
+  // Prints the distance on the Serial Monitor
+  Serial.print("Distance: ");
+  Serial.println(distance);
 
 }
--- motortest/motortt.ino
+++ motortest/motortt.ino
@@ -1,30 +1,29 @@
 
-int i;
 
-void move_till_switch(bool button){
-  if(button == false){
+void move_till(bool keepgoing){
+  if(keepgoing == true){
     for (i=0; i<255; i++) {
-      right_motor->setSpeed(i);
-      left_motor->setSpeed(i);
+      rightmotor->setSpeed(i);
+      leftmotor->setSpeed(i);
       delay(3);
     }
   }
   else{
     for (i=255; i!=0; i--) {
-      right_motor->setSpeed(i);
-      left_motor->setSpeed(i);
+      rightmotor->setSpeed(i);
+      leftmotor->setSpeed(i);
       delay(1);
     }
   }
 }
 
 // Main two motor controls
-void move_forward(bool front_switch){
-  right_motor->run(FORWARD);
-  left_motor->run(BACKWARD);
-  move_till_switch(front_switch);
+void move_forward(bool keepgoing){
+  rightmotor->run(FORWARD);
+  leftmotor->run(BACKWARD);
+  move_till(keepgoing);
 }
-
+/*
 void move_backward(bool back_switch){
   left_motor->run(FORWARD);
   right_motor->run(BACKWARD);
@@ -50,3 +49,4 @@ void lift_down(bool is_max_down){
     // lift goes down until hits bottom of the shaft
   }
 }
+*/
